1b37e97b01c6fd34760bd0f2cf3a765dbafb519c
sbogdan985 <bslaven@gmail.com>
2024-08-22 11:20:43 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
+  e2e: {
+    baseUrl: "https://automationteststore.com/",
+    excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/RegisterLogin.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe('Register and Login test suite', () => {
+
+it('test', () => {
+    cy.visit('/')
+
+
+})
+  
+
+
+})
